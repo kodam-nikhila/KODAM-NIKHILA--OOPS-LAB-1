faae8dc8fa88f8b490f9de8b47691b5cbecbe9d9
kodam-nikhila <nikhilakodam259@gmail.com>
2022-03-16 16:32:38 +0530

    lab

---
+++ src/com/greatlearning/emailapplication/MainClass.java
@@ -0,0 +1,13 @@
+package com.greatlearning.emailapplication;
+import com.greatlearning.emailapp.ui.EmailApplicationUI;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+EmailApplicationUI ui = new EmailApplicationUI();
+		
+		ui.init();
+	}
+
+}
